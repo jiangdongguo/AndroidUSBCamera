b686751ebf69ee9496ffa2ee41dd3b0de243a975
jiangdongguo <jiangdg@chinatelecom.cn>
2022-03-10 09:04:09 +0800

    [fix] quickly capture image crash

--- libuvc/src/main/jni/UVCCamera/UVCButtonCallback.cpp
+++ libuvc/src/main/jni/UVCCamera/UVCButtonCallback.cpp
@@ -71,15 +71,23 @@ void UVCButtonCallback::notifyButtonCallback(JNIEnv* env, int button, int state)
 }
 
 void UVCButtonCallback::uvc_button_callback(int button, int state, void *user_ptr) {
-
 	UVCButtonCallback *buttonCallback = reinterpret_cast<UVCButtonCallback *>(user_ptr);
-
 	JavaVM *vm = getVM();
 	JNIEnv *env;
-	// attach to JavaVM
-	vm->AttachCurrentThread(&env, NULL);
+	bool isAttached = false;
+
+	int status = vm->GetEnv((void**)&env, JNI_VERSION_1_6);
+	if (status < 0) {
+		// attach to JavaVM
+		if (vm->AttachCurrentThread(&env, NULL)) {
+			return;
+		}
+		isAttached = true;
+	}
 
 	buttonCallback->notifyButtonCallback(env, button, state);
-	
-	vm->DetachCurrentThread();
+
+	if (isAttached) {
+		vm->DetachCurrentThread();
+	}
 }
