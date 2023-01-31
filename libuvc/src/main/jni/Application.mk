3f0cabafbdc77c9ede3102da318560111961eea5
Joshua Monson <86740561+JMonson-SoftPoint@users.noreply.github.com>
2023-01-31 11:59:58 -0700

    x86_64 should be supported

--- libuvc/src/main/jni/Application.mk
+++ libuvc/src/main/jni/Application.mk
@@ -27,7 +27,7 @@
 #NDK_TOOLCHAIN_VERSION := 4.9
 
 APP_PLATFORM := android-14
-APP_ABI :=arm64-v8a armeabi-v7a
-#APP_ABI :=arm64-v8a armeabi-v7a x86 x86_64
+#APP_ABI :=arm64-v8a armeabi-v7a
+APP_ABI :=arm64-v8a armeabi-v7a x86 x86_64
 #APP_OPTIM := debug
 APP_OPTIM := release
