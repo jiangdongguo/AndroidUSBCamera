62d726380129ddec4bcb2df8846b66eb11a1bcef
RumitPatel <rumit.patel5@gmail.com>
2023-08-22 17:46:49 +0530

    SpaceItemDecoration.kt code formatted.

--- libausbc/src/main/java/com/jiangdg/ausbc/utils/SpaceItemDecoration.kt
+++ libausbc/src/main/java/com/jiangdg/ausbc/utils/SpaceItemDecoration.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
  *
  * @author Created by jiangdg on 2022/7/24
  */
-class SpaceItemDecoration(private val space: Int): RecyclerView.ItemDecoration() {
+class SpaceItemDecoration(private val space: Int) : RecyclerView.ItemDecoration() {
     override fun getItemOffsets(
         outRect: Rect,
         view: View,
@@ -17,7 +17,7 @@ class SpaceItemDecoration(private val space: Int): RecyclerView.ItemDecoration()
     ) {
         outRect.left = space
         outRect.bottom = space
-        if (parent.getChildLayoutPosition(view) %3==0) {
+        if (parent.getChildLayoutPosition(view) % 3 == 0) {
             outRect.left = 0
         }
     }
