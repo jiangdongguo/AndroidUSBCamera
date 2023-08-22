29a754d0966bde8f6b85beedee8fcad3379c6397
RumitPatel <rumit.patel5@gmail.com>
2023-08-22 17:46:18 +0530

    removed unnecessary semicolon.

--- libausbc/src/main/java/com/jiangdg/ausbc/utils/SpaceItemDecoration.kt
+++ libausbc/src/main/java/com/jiangdg/ausbc/utils/SpaceItemDecoration.kt
@@ -15,8 +15,8 @@ class SpaceItemDecoration(private val space: Int): RecyclerView.ItemDecoration()
         parent: RecyclerView,
         state: RecyclerView.State
     ) {
-        outRect.left = space;
-        outRect.bottom = space;
+        outRect.left = space
+        outRect.bottom = space
         if (parent.getChildLayoutPosition(view) %3==0) {
             outRect.left = 0
         }
