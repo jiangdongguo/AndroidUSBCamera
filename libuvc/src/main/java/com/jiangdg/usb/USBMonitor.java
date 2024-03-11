bd3c69023a9c278a6fe0ad88945307ca667a7b24
João Neves <joao.neves@outsystems.com>
2024-03-11 21:09:02 +0000

    Fix flag value

--- libuvc/src/main/java/com/jiangdg/usb/USBMonitor.java
+++ libuvc/src/main/java/com/jiangdg/usb/USBMonitor.java
@@ -177,7 +177,7 @@ public final class USBMonitor {
 					// avoid acquiring intent data failed in receiver on Android12
 					// when using PendingIntent.FLAG_IMMUTABLE
 					// because it means Intent can't be modified anywhere -- jiangdg/20220929
-					int PENDING_FLAG_IMMUTABLE = 1<<25;
+					int PENDING_FLAG_IMMUTABLE = 0x04000000;
 					mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), PENDING_FLAG_IMMUTABLE);
 				} else {
 					mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
